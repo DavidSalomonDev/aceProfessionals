5c1e1c202825a7988b37ff46b44a07a6df79f1c1
Vipansh Thakur <vipansh@github.com>
2021-01-10 12:35:42 +0530

    audio context dlt

--- src/componants/PopUpForm.js
+++ src/componants/PopUpForm.js
@@ -22,24 +22,24 @@ export const PopUpForm = (props) => {
     const { register, handleSubmit, errors } = useForm();
     const setModal = () => { props.setshowModal(false) }
 
-    const context = new window.AudioContext();
+    // const context = new window.AudioContext();
 
-    function playFile(filepath) {
-        // see https://jakearchibald.com/2016/sounds-fun/
-        // Fetch the file
-        fetch(filepath)
-            // Read it into memory as an arrayBuffer
-            .then(response => response.arrayBuffer())
-            // Turn it from mp3/aac/whatever into raw audio data
-            .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
-            .then(audioBuffer => {
-                // Now we're ready to play!
-                const soundSource = context.createBufferSource();
-                soundSource.buffer = audioBuffer;
-                soundSource.connect(context.destination);
-                soundSource.start();
-            });
-    }
+    // function playFile(filepath) {
+    //     // see https://jakearchibald.com/2016/sounds-fun/
+    //     // Fetch the file
+    //     fetch(filepath)
+    //         // Read it into memory as an arrayBuffer
+    //         .then(response => response.arrayBuffer())
+    //         // Turn it from mp3/aac/whatever into raw audio data
+    //         .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    //         .then(audioBuffer => {
+    //             // Now we're ready to play!
+    //             const soundSource = context.createBufferSource();
+    //             soundSource.buffer = audioBuffer;
+    //             soundSource.connect(context.destination);
+    //             soundSource.start();
+    //         });
+    // }
 
 
 
@@ -47,7 +47,7 @@ export const PopUpForm = (props) => {
         e.preventDefault()
         setShowAlert(true);
         saveUserData({ email: data.to_name, name: data.from_name, phone: data.phoneNo })
-        playFile('https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3');
+        // playFile('https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3');
 
         emailjs.sendForm("service_ggy36e6", "template_817ivy8", e.target, "user_qcVEGAk66Pd4cuX7BrWOi")
             .then((result) => {
